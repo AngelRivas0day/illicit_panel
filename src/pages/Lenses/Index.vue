d03830044024a04c30440147d3ad2086e730fa21
Angel <angelrivas0day@gmail.com>
2020-08-04 21:46:39 -0500

    started

---
+++ src/pages/Lenses/Index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="title">Lentes</h1>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-12">
+                    <base-button class="animation-on-hover" type="default">
+                        <i class="tim-icons icon-simple-add"></i>
+                        Agregar lente
+                    </base-button>
+                </div>
+                <div class="col-12">
+                    <table></table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Leneses',
+    data: ()=>({
+        title: 'Lenses'
+    })
+}
+</script>
+
+<style>
+
+</style>
